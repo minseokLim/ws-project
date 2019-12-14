89dc694f5791a76fe2b24fc599f019b43932e181
mslim <mslim8803@naver.com>
2019-12-14 19:31:37 +0900

    WS ownerEmail index 추가

--- src/main/java/com/wsproject/wsservice/domain/WS.java
+++ src/main/java/com/wsproject/wsservice/domain/WS.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Index;
 import javax.persistence.Table;
 
 import com.wsproject.wsservice.domain.enums.WS_Type;
@@ -16,7 +17,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor
-@Table(name = "TBL_WS")
+@Table(name = "TBL_WS", indexes = {@Index(columnList = "ownerEmail")})
 public class WS extends BaseTimeEntity {
 
 	@Id
