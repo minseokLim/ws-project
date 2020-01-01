4a737ac24f7f36b0d8b438ea2cef6a2d02334867
mslim <mslim8803@naver.com>
2020-01-01 16:23:16 +0900

    ws-service, confsvr 유레카서버에 등록

--- ws-service/src/main/docker/run.sh
+++ ws-service/src/main/docker/run.sh
@@ -1,6 +1,12 @@
  #!/bin/sh
 
 echo "********************************************************"
+echo "Waiting for the Eureka server to start on port $EUREKASERVER_PORT"
+echo "********************************************************"
+while ! `nc -z eurekasvr $EUREKASERVER_PORT`; do sleep 3; done
+echo "Eureka Server has started"
+
+echo "********************************************************"
 echo "Waiting for the Configuration server to start on port $CONFIGSERVER_PORT"
 echo "********************************************************"
 while ! `nc -z confsvr $CONFIGSERVER_PORT`; do sleep 3; done
@@ -18,4 +24,5 @@ echo "********************************************************"
 java -Djava.security.egd=file:/dev/./urandom \
 	 -Dspring.profiles.active=$PROFILE \
 	 -Dspring.cloud.config.uri=$CONFIGSERVER_URI \
+	 -Deureka.client.serviceUrl.defaultZone=$EUREKASERVER_URI \
 	 -jar /usr/local/ws-service/@project.build.finalName@.jar
