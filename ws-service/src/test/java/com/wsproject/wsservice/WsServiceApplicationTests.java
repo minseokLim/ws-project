0dfe13315fe84d07699dfa9e5db8ac14f4247ec3
mslim <mslim8803@naver.com>
2020-01-01 20:34:37 +0900

    zuul server docker 설정. ws-service Test code에서 url 변경

--- ws-service/src/test/java/com/wsproject/wsservice/WsServiceApplicationTests.java
+++ ws-service/src/test/java/com/wsproject/wsservice/WsServiceApplicationTests.java
@@ -46,11 +46,11 @@ class WsServiceApplicationTests {
 	@Test
 	void contextLoads() throws Exception {
 		Ws ws = Ws.builder().content("Let it be").author("Beatles").type(WsType.ETC).build();
-		String jsonStr = mockMvc.perform(post("/v1.0/api/wses").contentType(MediaType.APPLICATION_JSON)
+		String jsonStr = mockMvc.perform(post("/v1.0/wses").contentType(MediaType.APPLICATION_JSON)
 				.content(objectMapper.writeValueAsString(ws))).andExpect(status().isCreated()).andReturn().getResponse().getContentAsString();
 		Long id = objectMapper.readValue(jsonStr, Ws.class).getId();
 		
-		jsonStr = mockMvc.perform(get("/v1.0/api/wses/" + id).accept(MediaType.APPLICATION_JSON)).andReturn().getResponse().getContentAsString();
+		jsonStr = mockMvc.perform(get("/v1.0/wses/" + id).accept(MediaType.APPLICATION_JSON)).andReturn().getResponse().getContentAsString();
 		Ws saved = objectMapper.readValue(jsonStr, Ws.class);
 		LocalDateTime createdDate = saved.getCreatedDate();
 		LocalDateTime modifiedDate = saved.getModifiedDate();
@@ -60,9 +60,9 @@ class WsServiceApplicationTests {
 		Ws newWs = Ws.builder().content("Hey Jude").author("Beatles").type(WsType.ETC).build();
 		saved.update(newWs);
 		
-		mockMvc.perform(put("/v1.0/api/wses/" + id).contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(saved))).andExpect(status().isOk());
+		mockMvc.perform(put("/v1.0/wses/" + id).contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(saved))).andExpect(status().isOk());
 		
-		jsonStr = mockMvc.perform(get("/v1.0/api/wses/" + id).accept(MediaType.APPLICATION_JSON)).andReturn().getResponse().getContentAsString();
+		jsonStr = mockMvc.perform(get("/v1.0/wses/" + id).accept(MediaType.APPLICATION_JSON)).andReturn().getResponse().getContentAsString();
 		Ws updated = objectMapper.readValue(jsonStr, Ws.class);
 		
 		assertTrue("Hey Jude".equals(updated.getContent()));
--- zuulsvr/src/main/docker/run.sh
+++ zuulsvr/src/main/docker/run.sh
@@ -1,1 +1,14 @@
 #!/bin/sh
+
+echo "********************************************************"
+echo "Waiting for the Eureka server to start on port $EUREKASERVER_PORT"
+echo "********************************************************"
+while ! `nc -z eurekasvr $EUREKASERVER_PORT`; do sleep 3; done
+echo "Eureka Server has started"
+
+echo "********************************************************"
+echo "Starting Zuul Server"
+echo "********************************************************"
+java -Djava.security.egd=file:/dev/./urandom \
+	 -Deureka.client.serviceUrl.defaultZone=$EUREKASERVER_URI \
+	 -jar /usr/local/zuulsvr/@project.build.finalName@.jar
