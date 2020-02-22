9a1494f1d66b6f784feaf78ddeabe0547bb03b94
mslim <mslim8803@naver.com>
2020-02-22 21:27:32 +0900

    authsvr mvn package 시 test error나는 문제 수정
    application.yml 일부 초기값 추가

--- authsvr/src/test/java/com/wsproject/authsvr/AuthsvrApplicationTests.java
+++ authsvr/src/test/java/com/wsproject/authsvr/AuthsvrApplicationTests.java
@@ -1,18 +1,13 @@
 package com.wsproject.authsvr;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootTest
 class AuthsvrApplicationTests {
 	
-	@Autowired
-	private PasswordEncoder passwordEncoder;
-	
 	@Test
 	void test() throws Exception {
-		System.out.println("testSecret : " + passwordEncoder.encode("zq8WAZ5V9GVQK6COD2TQSfvOzExRibD4"));
+		
 	}
 }
