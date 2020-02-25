acfc58e03684ba73eebfbbd0a0a8bc53d27c46e6
mslim <mslim8803@naver.com>
2020-02-25 19:38:18 +0900

    scope명 mobile로 변경

--- user-service/src/main/java/com/wsproject/userservice/config/ResourceServerConfig.java
+++ user-service/src/main/java/com/wsproject/userservice/config/ResourceServerConfig.java
@@ -23,7 +23,7 @@ public class ResourceServerConfig  extends ResourceServerConfigurerAdapter {
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-			.antMatchers("/v1.0/users/me").access("#oauth2.hasScope('profile')")
+			.antMatchers("/v1.0/users/me").access("#oauth2.hasScope('mobile')")
 			.anyRequest().authenticated();
 	}
 	 
