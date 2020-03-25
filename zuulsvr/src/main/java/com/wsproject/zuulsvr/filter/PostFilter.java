521165f8422f0122878805808157db6d2c67258a
mslim <mslim8803@naver.com>
2020-03-25 21:15:11 +0900

    상관관계 아이디 response에 넣는 기능 버그 수정

--- zuulsvr/src/main/java/com/wsproject/zuulsvr/filter/PostFilter.java
+++ zuulsvr/src/main/java/com/wsproject/zuulsvr/filter/PostFilter.java
@@ -1,5 +1,7 @@
 package com.wsproject.zuulsvr.filter;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Component;
 
 import com.netflix.zuul.ZuulFilter;
@@ -8,11 +10,11 @@ import com.netflix.zuul.exception.ZuulException;
 import com.wsproject.zuulsvr.constant.FilterType;
 
 import brave.Tracer;
-import lombok.RequiredArgsConstructor;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Component
-@RequiredArgsConstructor
+@AllArgsConstructor
 @Slf4j
 public class PostFilter extends ZuulFilter {
 	private static final int FILTER_ORDER = 1;
@@ -39,9 +41,10 @@ public class PostFilter extends ZuulFilter {
 	@Override
 	public Object run() throws ZuulException {
 		RequestContext ctx = RequestContext.getCurrentContext();
-		
-		// 상관관계 ID 추가
-		ctx.getResponse().addHeader("trace-id", tracer.currentSpan().context().traceIdString());
+
+		HttpServletResponse response = ctx.getResponse();
+		String traceId = tracer.currentSpan().context().traceIdString();
+		response.addHeader("x-b3-traceid", traceId);
 		
 		log.info("Outgoing request for {}", ctx.getRequest().getRequestURI());
 		return null;
