e8b182739d232712748808242202f69ef7621a36
mslim <mslim8803@naver.com>
2020-03-25 20:22:44 +0900

    spring cloud sleuth 에서 추가되는 traceId를 zuul Postfilter에서 response에 추가

--- zuulsvr/src/main/java/com/wsproject/zuulsvr/filter/PostFilter.java
+++ zuulsvr/src/main/java/com/wsproject/zuulsvr/filter/PostFilter.java
@@ -7,14 +7,19 @@ import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
 import com.wsproject.zuulsvr.constant.FilterType;
 
+import brave.Tracer;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Component
+@RequiredArgsConstructor
 @Slf4j
 public class PostFilter extends ZuulFilter {
 	private static final int FILTER_ORDER = 1;
 	private static final boolean SHOULD_FILTER = true;
 	private static final String FILTER_TYPE = FilterType.POST_FILTER_TYPE;
+	
+	private Tracer tracer;
 	
 	@Override
 	public int filterOrder() {
@@ -34,6 +39,10 @@ public class PostFilter extends ZuulFilter {
 	@Override
 	public Object run() throws ZuulException {
 		RequestContext ctx = RequestContext.getCurrentContext();
+		
+		// 상관관계 ID 추가
+		ctx.getResponse().addHeader("trace-id", tracer.currentSpan().context().traceIdString());
+		
 		log.info("Outgoing request for {}", ctx.getRequest().getRequestURI());
 		return null;
 	}
