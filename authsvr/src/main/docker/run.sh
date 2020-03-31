e26aea9bc1d055a8a4eee015707d6fd9421a5276
mslim <mslim8803@naver.com>
2020-03-31 20:03:15 +0900

    AWS에 배포 가능하게 수정

--- authsvr/src/main/docker/run.sh
+++ authsvr/src/main/docker/run.sh
@@ -1,22 +1,19 @@
 #!/bin/sh
+getPort() {
+    echo $1 | cut -d : -f 3 | xargs basename
+}
 
 echo "********************************************************"
-echo "Waiting for the Eureka server to start on port $EUREKASERVER_PORT"
+echo "Waiting for the Eureka server to start on port $(getPort $EUREKASERVER_PORT)"
 echo "********************************************************"
-while ! `nc -z eurekasvr $EUREKASERVER_PORT`; do sleep 3; done
+while ! `nc -z eurekasvr $(getPort $EUREKASERVER_PORT)`; do sleep 3; done
 echo "Eureka Server has started"
 
 echo "********************************************************"
-echo "Waiting for the Configuration server to start on port $CONFIGSERVER_PORT"
+echo "Waiting for the Configuration server to start on port $(getPort $CONFIGSERVER_PORT)"
 echo "********************************************************"
-while ! `nc -z confsvr $CONFIGSERVER_PORT`; do sleep 3; done
+while ! `nc -z confsvr $(getPort $CONFIGSERVER_PORT)`; do sleep 3; done
 echo "Configuration Server has started"
-
-echo "********************************************************"
-echo "Waiting for the Database server to start on port $DATABASESERVER_PORT"
-echo "********************************************************"
-while ! `nc -z database $DATABASESERVER_PORT`; do sleep 3; done
-echo "Database Server has started"
 
 echo "********************************************************"
 echo "Starting Authorization Server"
--- batch-service/src/main/docker/run.sh
+++ batch-service/src/main/docker/run.sh
@@ -1,27 +1,24 @@
 #!/bin/sh
+getPort() {
+    echo $1 | cut -d : -f 3 | xargs basename
+}
 
 echo "********************************************************"
-echo "Waiting for the Configuration server to start on port $CONFIGSERVER_PORT"
+echo "Waiting for the Configuration server to start on port $(getPort $CONFIGSERVER_PORT)"
 echo "********************************************************"
-while ! `nc -z confsvr $CONFIGSERVER_PORT`; do sleep 3; done
+while ! `nc -z confsvr $(getPort $CONFIGSERVER_PORT)`; do sleep 3; done
 echo "Configuration Server has started"
 
 echo "********************************************************"
-echo "Waiting for the Database server to start on port $DATABASESERVER_PORT"
+echo "Waiting for the WS Service to start on port $(getPort $WS_SERVICE_PORT)"
 echo "********************************************************"
-while ! `nc -z database $DATABASESERVER_PORT`; do sleep 3; done
-echo "Database Server has started"
-
-echo "********************************************************"
-echo "Waiting for the WS Service to start on port $WS_SERVICE_PORT"
-echo "********************************************************"
-while ! `nc -z ws-service $WS_SERVICE_PORT`; do sleep 3; done
+while ! `nc -z ws-service $(getPort $WS_SERVICE_PORT)`; do sleep 3; done
 echo "WS Service has started"
 
 echo "********************************************************"
-echo "Waiting for the User Service to start on port $USER_SERVICE_PORT"
+echo "Waiting for the User Service to start on port $(getPort $USER_SERVICE_PORT)"
 echo "********************************************************"
-while ! `nc -z user-service $USER_SERVICE_PORT`; do sleep 3; done
+while ! `nc -z user-service $(getPort $USER_SERVICE_PORT)`; do sleep 3; done
 echo "User Service has started"
 
 echo "********************************************************"
