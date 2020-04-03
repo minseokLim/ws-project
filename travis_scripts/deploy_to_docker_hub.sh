54748ad126e5576f1ea74430717d618e6b699c02
mslim <mslim8803@naver.com>
2020-04-03 19:49:00 +0900

    Travis를 이용한 CI/CD 설정. 과연 될 것인그아....

--- travis_scripts/deploy_to_docker_hub.sh
+++ travis_scripts/deploy_to_docker_hub.sh
@@ -1,9 +1,9 @@
 echo "Pushing service docker images to docker hub ...."
-docker login
-docker push mslim8803/authsvr
-docker push mslim8803/batch-service
-docker push mslim8803/confsvr
-docker push mslim8803/eurekasvr
-docker push mslim8803/user-service
-docker push mslim8803/ws-service
-docker push mslim8803/zuulsvr
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+docker push mslim8803/authsvr:$BUILD_NAME
+# docker push mslim8803/batch-service:$BUILD_NAME
+docker push mslim8803/confsvr:$BUILD_NAME
+docker push mslim8803/eurekasvr:$BUILD_NAME
+docker push mslim8803/user-service:$BUILD_NAME
+docker push mslim8803/ws-service:$BUILD_NAME
+docker push mslim8803/zuulsvr:$BUILD_NAME
