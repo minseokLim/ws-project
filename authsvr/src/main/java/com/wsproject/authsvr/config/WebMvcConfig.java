1991399afdca121e1498ccca7f6c7f7c9928a3d9
mslim <mslim8803@naver.com>
2020-04-08 08:22:19 +0900

    인증서버 로그아웃 되지 않는 문제 디버깅 위해 로그 추가

--- authsvr/src/main/java/com/wsproject/authsvr/config/WebMvcConfig.java
+++ authsvr/src/main/java/com/wsproject/authsvr/config/WebMvcConfig.java
@@ -1,20 +1,21 @@
 package com.wsproject.authsvr.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 	
-//	private static final long MAX_AGE_SECONDS = 3600;
-//
-//	@Override
-//	public void addCorsMappings(CorsRegistry registry) {
-//		registry.addMapping("/**")
-//				.allowedOrigins("*")
-//				.allowedMethods("GET", "POST", "PUT", "DELETE")
-//				.allowedHeaders("*")
-//				.allowCredentials(true)
-//				.maxAge(MAX_AGE_SECONDS);
-//	}
+	private static final long MAX_AGE_SECONDS = 0;
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedOrigins("*")
+				.allowedMethods("GET", "POST", "PUT", "DELETE")
+				.allowedHeaders("*")
+				.allowCredentials(true)
+				.maxAge(MAX_AGE_SECONDS);
+	}
 }
