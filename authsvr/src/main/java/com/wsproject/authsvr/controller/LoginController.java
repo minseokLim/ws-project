118e5f1ee08f7e225c521453476954aa5f98dc8e
mslim <mslim8803@naver.com>
2020-04-08 14:46:23 +0900

    Lim

--- authsvr/src/main/java/com/wsproject/authsvr/controller/LoginController.java
+++ authsvr/src/main/java/com/wsproject/authsvr/controller/LoginController.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
