200d80eac8da02d31dfaacbc03cbe5e88d4cd6ce
mslim <mslim8803@naver.com>
2020-04-09 20:34:43 +0900

    딱히 쓸 말이 없다. 무분별한 푸시를 지양해야겠다. 앞으로는 잘 좀 합시다

--- authsvr/src/main/java/com/wsproject/authsvr/config/WebMvcConfig.java
+++ authsvr/src/main/java/com/wsproject/authsvr/config/WebMvcConfig.java
@@ -1,22 +1,13 @@
 package com.wsproject.authsvr.config;
 
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import com.wsproject.authsvr.util.SessionInvalidateFilter;
-
-import lombok.AllArgsConstructor;
-
-@AllArgsConstructor
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 	
 	private static final long MAX_AGE_SECONDS = 0;
-	
-	private SessionInvalidateFilter sessionInvalidateFilter;
 	
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
@@ -26,13 +17,5 @@ public class WebMvcConfig implements WebMvcConfigurer {
 				.allowedHeaders("*")
 				.allowCredentials(true)
 				.maxAge(MAX_AGE_SECONDS);
-	}
-	
-	@Bean
-	public FilterRegistrationBean<SessionInvalidateFilter> filterRegistrationBean() {
-		FilterRegistrationBean<SessionInvalidateFilter> registrationBean = new FilterRegistrationBean<>();
-		registrationBean.setFilter(sessionInvalidateFilter);
-		registrationBean.addUrlPatterns("/oauth/authorize*");
-		return registrationBean;
 	}
 }
