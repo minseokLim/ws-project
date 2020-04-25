320d930ca5f6683d8696730eac616e6e90b96f85
mslim <mslim8803@naver.com>
2020-04-25 22:29:55 +0900

    ws-service에서 관리자만 접근할 수 있는 서비스 명시

--- ws-service/src/main/java/com/wsproject/wsservice/config/ResourceServerConfig.java
+++ ws-service/src/main/java/com/wsproject/wsservice/config/ResourceServerConfig.java
@@ -20,7 +20,8 @@ public class ResourceServerConfig  extends ResourceServerConfigurerAdapter {
 	
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()	
+		http.authorizeRequests()
+			.antMatchers("/v1.0/wses/**").hasRole("ADMIN") // 관리자만 명언서비스(각 사용자별 명언이 아닌 모든 사용자에게 노출되는 명언)에 접근할 수 있음
 			.anyRequest().authenticated();
 	}
 	 
