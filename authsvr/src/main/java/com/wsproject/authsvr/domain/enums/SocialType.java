2f3d81d6670a9ad9eb27ce59410cf41c5ae9be61
mslim <mslim8803@naver.com>
2020-07-05 18:40:17 +0900

    enum에 Converter 적용

--- authsvr/src/main/java/com/wsproject/authsvr/domain/enums/SocialType.java
+++ authsvr/src/main/java/com/wsproject/authsvr/domain/enums/SocialType.java
@@ -1,5 +1,8 @@
 package com.wsproject.authsvr.domain.enums;
 
+import java.util.Arrays;
+
+import lombok.Getter;
 
 /**
  * Social 서비스 종류 <br>
@@ -7,19 +10,25 @@ package com.wsproject.authsvr.domain.enums;
  * @author mslim
  *
  */
+@Getter
 public enum SocialType {
-	FACEBOOK("facebook"),
-	GOOGLE("google"),
-	KAKAO("kakao"),
-	GITHUB("github");
+	FACEBOOK("facebook", 0),
+	GOOGLE("google", 1),
+	KAKAO("kakao", 2),
+	GITHUB("github", 3);
 	
-	private String name;
+	private String value;
+	private int code;
 	
-	SocialType(String name) {
-		this.name = name;
+	private SocialType(String value, int code) {
+		this.value = value;
+		this.code = code;
 	}
 	
-	public String getValue() {
-		return name;
+	public static SocialType ofCode(int code) {
+		return Arrays.stream(SocialType.values())
+					.filter(v -> v.getCode() == code)
+					.findAny()
+					.orElseThrow(() -> new RuntimeException(String.format("상태코드에 code=[%s]가 존재하지 않습니다.", code)));
 	}
 }
--- ws-service/src/main/java/com/wsproject/wsservice/domain/enums/WsType.java
+++ ws-service/src/main/java/com/wsproject/wsservice/domain/enums/WsType.java
@@ -1,11 +1,33 @@
 package com.wsproject.wsservice.domain.enums;
 
+import java.util.Arrays;
+
+import lombok.Getter;
+
 /**
  * 명언 종류<br>
  * 자기 개발, YOLO 등으로 명언의 종류를 구분해보려고 했으나, 구분하는 기준이 주관적일 수 있어서 현재는 거의 고려하고 있지 않은 상태
  * @author mslim
  *
  */
+@Getter
 public enum WsType {
-	SELF_DEV, YOLO, ETC
+	SELF_DEV("자기개발", 0), 
+	YOLO("YOLO", 1), 
+	ETC("기타", 2);
+	
+	private String desc;
+	private int code;
+	
+	private WsType(String desc, int code) {
+		this.desc = desc;
+		this.code = code;
+	}
+	
+	public static WsType ofCode(int code) {
+		return Arrays.stream(WsType.values())
+					.filter(v -> v.getCode() == code)
+					.findAny()
+					.orElseThrow(() -> new RuntimeException(String.format("상태코드에 code=[%s]가 존재하지 않습니다.", code)));
+	}
 }
